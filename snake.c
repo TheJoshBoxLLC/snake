4152cfe851abef85a24a86a5092de5340136fc32
Ben Carlson <bjcarlson42@outlook.com>
2021-11-27 20:23:11 -0500

    update: everything except trophies.

--- snake.c
+++ snake.c
@@ -6,6 +6,31 @@
 * To compile: gcc -o snake snake.c -lcurses
 * To run: ./snake
 * To terminate: press command + c
+*
+* The snake pit:
+* [X] The snake pit must utilize all available space of the current terminal window.
+* [X] There must be a visible border delineating the snake pit.
+* 
+* The snake:
+* [X] The initial length of the snake is three characters.
+* [X] Initial direction of the snake's movement is chosen randomly.
+* [X] The user can press one of the four arrow keys to change the direction of the snake's movement.
+* [X] The snake's speed is proportional to its length.
+* 
+* The trophies:
+* [ ] Trophies are represented by a digit randomly chosen from 1 to 9.
+* [ ] There's always exactly one trophy in the snake pit at any given moment.
+* [ ] When the snake eats the trophy, its length is increased by the corresponding number of characters.
+* [ ] A trophy expires after a random interval from 1 to 9 seconds.
+* [ ] A new trophy is shown at a random location on the screen after the previous one has either expired or is eaten by the snake.
+* 
+* The gameplay:
+* The snake dies and the game ends if:
+* [X] It runs into the border; or
+* [X] It runs into itself; or
+* [X] The user attempts to reverse the snake's direction.
+* 
+* [X] The user wins the game if the snake's length grows to the length equal to half the perimeter of the border.
 */
 
 #include <ncurses.h>
@@ -24,12 +49,28 @@
 #define STDIN_FD 0 // standard input file descriptor
 // end constants *************
 
+// ************* function prototypes ************* //
+void init_pit_border();
+void init_snake(int, int);
+void move_snake();
+void grow_snake(int);
+char choose_random_direction();
+void run_when_time_runs_out();
+void set_nodelay();
+void end_snake_game(int signum);
+void detect_collisions();
+void check_win();
+int calculate_snake_length();
+// end function prototypes *************
+
 // ************* variables ************* //
+int win_length = 0;         // length of snake required to win
 int row = 0;                // current row
 int col = 0;                // current column
 char key = 'd';             // key input
-short ticks = 0;            // ticks make up the game clock. ticks will eventually get faster when trophies are introduced
-short timeUnit = 128;       // timeUnit is made up of ticks
+short ticks = 0;            // ticks make up the game clock. They count to the timeUnit at a constant rate. when timeUnit gets smaller, ticks reach it faster meaning snake speeds up.
+short timeUnitConstant = 0; // equal to the initial timeUnit. Unlike timeUnit, this variable will not change.
+short timeUnit = 0;         // timeUnit is made up of ticks. The lower the time unit, the faster the snake will move.
 short directionY = 0;       // y direction
 short directionX = 0;       // x direction
 struct timespec speed, rem; // refresh rate
@@ -46,19 +87,9 @@ struct node *head; // snake head
 struct node *tail; // snake tail
 // end variables *************
 
-// ************* function prototypes ************* //
-void init_pit_border();
-void init_snake(int, int);
-void move_snake();
-void grow_snake(int);
-char choose_random_direction();
-void run_when_time_runs_out();
-void set_nodelay();
-void end_snake_game(int signum);
-// end function prototypes *************
-
 int main()
 {
+    srand((unsigned)time(NULL));     // seed random number generator
     signal(SIGINT, end_snake_game);  // when user terminates program, end game!
     initscr();                       // start curses
     clear();                         // clear the screen
@@ -75,7 +106,9 @@ int main()
     snake_head_y = row / 2;
     snake_head_x = col / 2;
     init_snake(snake_head_y, snake_head_x); // initialize snake of size 5 in the center of the screen
-
+    win_length = (2 * (LINES + COLS)) / 2;  // calculate length of snake required to win
+    timeUnit = 40 + win_length;             // set time unit to 40 plus the length to win. This is because we want the use to end on a timeUnit of 40.
+    timeUnitConstant = timeUnit;            // set time unit constant to time unit. This is used to increase snake speed.
     // Print welcome message to center of screen
     move(row / 2, col / 2 - 50 / 2);
     addstr("Welcome to our snake game! Press any key to start.");
@@ -92,7 +125,7 @@ int main()
     addstr("                                                  ");
     move(row - 1, col - 1);
 
-    // start game and loop until user terminates
+    // start game and loop until user terminates, wins, or loses
     // By: Benjamin Carlson
     while (1)
     {
@@ -107,6 +140,8 @@ int main()
             }
         }
 
+        // if user presses a key, change direction
+        // By: Benjamin Carlson
         switch (input) // if input is a valid key i.e - arrow key or left arrow keys, set direction
         {
         case (char)KEY_LEFT: // left
@@ -153,8 +188,6 @@ int main()
 */
 void init_pit_border()
 {
-    addstr("Welcome to our snake game! This is where trophy and other info will go\n");
-
     for (int i = 1; i < LINES; i++)
     { // outer loop
         for (int j = 0; j < COLS; j++)
@@ -170,7 +203,7 @@ void init_pit_border()
 }
 
 /*
-* Snake
+* Snake - Initializes the snake.
 * By: Benjamin Carlson
 * What it does: Initializes the snake. The snake is a linked list of nodes.
 * The head and tail of the snake are the first and last nodes in the linked list.
@@ -188,7 +221,7 @@ void init_snake(int start_y, int start_x)
 }
 
 /*
-* Snake
+* Snake - Grow Snake
 * By: Benjamin Carlson
 * What it does: Grow the snake by a specified amount.
 */
@@ -209,7 +242,7 @@ void grow_snake(int num_to_grow_by)
 }
 
 /*
-* Snake
+* Snake - Move Snake
 * By: Benjamin Carlson
 * What it does: Move the snake.
 */
@@ -220,7 +253,9 @@ void move_snake()
     head->row += directionY;           // move snake head in direction of directionY
     head->column += directionX;        // move snake head in direction of directionX
     move(head->row, head->column);     // move cursor to new position
+    attron(A_STANDOUT);
     addstr("O");                       // add "O" to the screen. This is the snake head
+    attroff(A_STANDOUT);
     struct node *scanner = head->prev; // scanner is the node that will be removed from the snake
     while (scanner != NULL)            // move all nodes in the snake
     {
@@ -239,16 +274,57 @@ void move_snake()
 }
 
 /*
+* Snake - Length
+* By: Benjamin Carlson
+* What it does: A helper function to calculate the snake length.
+*/
+int calculate_snake_length()
+{
+    int length = 0;
+    struct node *scanner = head;
+    while (scanner != NULL)
+    {
+        length++;
+        scanner = scanner->prev;
+    }
+    return length;
+}
+
+/*
 * Snake Direction
 * By: Benjamin Carlson
 * What it does: Set the initial direction of the snake.
 */
 char choose_random_direction()
 {
-    // this will eventually be the random direction. For first deliverable it will be to the right
-    directionY = 0;
-    directionX = 1;
-    return 'd';
+    // choose random direction and set the direction variables. Doesn't matter which number corresponds to which direction.
+    int random_direction = rand() % 4;
+    switch (random_direction)
+    {
+    case 0:
+        directionY = 0;
+        directionX = -1;
+        return 'a';
+        break;
+    case 1:
+        directionY = 0;
+        directionX = 1;
+        return 'd';
+        break;
+    case 2:
+        directionY = -1;
+        directionX = 0;
+        return 'w';
+        break;
+    case 3:
+        directionY = 1;
+        directionX = 0;
+        return 's';
+        break;
+    default:
+        return ' '; // should never happen
+        break;
+    }
 }
 
 /*
@@ -259,9 +335,23 @@ char choose_random_direction()
 */
 void run_when_time_runs_out()
 {
-    ticks = 0;    // reset ticks
-    move_snake(); // move snake
-    refresh();    // buffer -> screen
+    check_win(); // check if the snake has won
+    // get random number from 1 to 5. If number is 1, then the snake will grow.
+    int random_number = rand() % 5;
+    if (random_number == 1)
+        grow_snake(1);                                      // grow snake by 1, testing purposes
+    timeUnit = timeUnitConstant - calculate_snake_length(); // time unit is current time unit - snake length. This speeds up the snake as it gets longer.
+    ticks = 0;                                              // reset ticks
+    move_snake();                                           // move snake
+    detect_collisions();                                    // detect collisions
+    move(0, 0);
+    addstr("Length To Win: ");
+    printw("%d", win_length);
+    addstr("  Current Snake Length: ");
+    printw("%d", calculate_snake_length());
+    addstr("  Snake Speed: ");
+    printw("%d", timeUnitConstant - timeUnit);
+    refresh(); // buffer -> screen
 }
 
 /*
@@ -286,4 +376,76 @@ void end_snake_game(int signum)
 {
     endwin(); // end curses mode
     exit(1);  // exit program
+}
+
+/*
+* Detect Collisions
+* By: Benjamin Carlson
+* What it does: Detects collisions.
+* The snake dies and the game ends if:
+*  - It runs into the border; or
+*  - It runs into itself; or
+*  - The user attempts to reverse the snake's direction.
+*/
+void detect_collisions()
+{
+    // detect if snake runs into border
+    if (head->row == 1 || head->row == LINES - 1 || head->column == 0 || head->column == COLS - 1)
+    {
+        // if user runs into a border, write message to screen and end game
+        move(row / 2, col / 2);
+        attron(A_STANDOUT);
+        addstr("You ran into a border!");
+        attroff(A_STANDOUT);
+        refresh();
+        sleep(2);
+        raise(SIGINT); // raise interrupt signal to end game
+    }
+    struct node *scanner = head->prev; // scanner is the node that will be removed from the snake
+    while (scanner != NULL)            // move all nodes in the snake
+    {
+        // detect if snake runs into itself (also handles if snake reverses direction)
+        if (head->row == scanner->row && head->column == scanner->column)
+        {
+            // if user runs into itself, write message to screen and end game
+            move(row / 2, col / 2);
+            attron(A_STANDOUT);
+            addstr("You ran into yourself!");
+            attroff(A_STANDOUT);
+            refresh();
+            sleep(2);
+            raise(SIGINT); // raise interrupt signal to end game
+        }
+        scanner = scanner->prev; // move scanner to the previous node
+    }
+}
+
+/*
+* Win condition
+* By: Benjamin Carlson
+* What it does: Checks if the snake is the required length to win.
+*/
+void check_win()
+{
+    // calculate half the perimeter of the border -> P=(2(l+w)) / 2
+    win_length = (2 * (LINES + COLS)) / 2;
+    // move to top right corner and write the win_length
+    move(0, 0);
+    printw("%d", win_length);
+
+    int length = calculate_snake_length(); // calculate length of snake
+    // move(row / 2, col / 2);
+    // printw("%d", length);
+
+    // if snake is the required length, write message to screen and end game
+    if (length >= win_length) // >= because we might go over. Ex, snake is one away from winning but eats a trophy of size 9
+    {
+        move(row / 2, col / 2);
+        attron(A_STANDOUT);
+        addstr("You won!");
+        attroff(A_STANDOUT);
+        refresh();
+        sleep(2);
+        raise(SIGINT); // raise interrupt signal to end game
+    }
 }
